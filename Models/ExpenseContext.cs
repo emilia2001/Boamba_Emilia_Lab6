f2b21e7f34b210d0d7566f5eaff737da9e522b2d
emilia2001 <emilia.boamba@gmail.com>
2023-10-25 20:30:48 +0300

    Add project files.

---
+++ Models/ExpenseContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Boamba_Emilia_Lab6.Models
+{
+    public class ExpenseContext : DbContext
+    {
+        public ExpenseContext(DbContextOptions<ExpenseContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Expense> Expense { get; set; }
+        public DbSet<ExpenseDTO> ExpenseDTO { get; set; }
+
+    }
+
+}
